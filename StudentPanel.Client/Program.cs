a6a33822840ede887c9b3f8499f44170256bf7e6
Arsentii Turlo <s34233@pjwstk.edu.pl>
2026-06-09 03:19:15 +0200

    base setup, HttpClient registration, DI services

--- StudentPanel.Client/Program.cs
+++ StudentPanel.Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StudentPanel.Client;
+using StudentPanel.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<StudentsApiClient>(client => client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!));
+
+builder.Services.AddScoped<ObservedStudentsState>();
 
 await builder.Build().RunAsync();
